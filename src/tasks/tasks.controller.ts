edcb98eb1a57d118beb036b4107efdced702d6a9
Paulo Santos <paulovictormottasantos@hotmail.com>
2023-03-05 10:41:51 -0300

    feat(tasks): implement create task route

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,7 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Param, Post } from '@nestjs/common';
+import { CreateTaskDto } from './dtos/create-task.dto';
 import { TasksService } from './tasks.service';
 
-@Controller('tasks')
+@Controller('task')
 export class TasksController {
   constructor(private readonly tasksService: TasksService) {}
+
+  @Post('/create/:accountId')
+  async createTask(
+    @Param('accountId') accountId: string,
+    @Body() task: CreateTaskDto,
+  ) {
+    return await this.tasksService.createTask(accountId, task);
+  }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,36 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateTaskDto } from './dtos/create-task.dto';
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async createTask(accountId: string, task: CreateTaskDto) {
+    const foundAccount = await this.prismaService.account.findUnique({
+      where: {
+        id: accountId,
+      },
+    });
+
+    if (!foundAccount) {
+      throw new HttpException('Account not found.', HttpStatus.NOT_FOUND);
+    }
+
+    const createdTask = await this.prismaService.task.create({
+      data: {
+        content: task.content,
+        accountId,
+      },
+    });
+
+    if (!createdTask) {
+      throw new HttpException(
+        'Task not created.',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+
+    throw new HttpException('Task successfully created.', HttpStatus.CREATED);
+  }
+}
