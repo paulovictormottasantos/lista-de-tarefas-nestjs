9781345ffdc5dcf5c748e69f117d6b9b84a8e041
Paulo Santos <paulovictormottasantos@hotmail.com>
2023-03-05 10:07:27 -0300

    build(accounts and tasks): generate resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
+import { AccountsModule } from './accounts/accounts.module';
+import { TasksModule } from './tasks/tasks.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, AccountsModule, TasksModule],
 })
 export class AppModule {}
